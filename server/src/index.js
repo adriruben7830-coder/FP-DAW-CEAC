47e28924cd5fdb3b3c52cf0f8fd73ef2a028a5aa
adriruben7830-coder <adriruben7830@gmail.com>
2026-03-23 11:41:15 +0100

    Swagger CDN para Vercel serverless

--- server/src/index.js
+++ server/src/index.js
@@ -24,7 +24,13 @@ const loggerAcademico = (req, res, next) => {
 app.use(loggerAcademico);
 
 // Swagger
-app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerSpec));
+app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerSpec, {
+  customCssUrl: 'https://cdnjs.cloudflare.com/ajax/libs/swagger-ui/5.11.0/swagger-ui.min.css',
+  customJs: [
+    'https://cdnjs.cloudflare.com/ajax/libs/swagger-ui/5.11.0/swagger-ui-bundle.min.js',
+    'https://cdnjs.cloudflare.com/ajax/libs/swagger-ui/5.11.0/swagger-ui-standalone-preset.min.js'
+  ]
+}));
 
 // Rutas
 app.use('/api/v1/tasks', taskRoutes);
